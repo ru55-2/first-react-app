5f0d59c12dab7c0a41c49c761e5cad47ca2e376d
ru55-2 <rm_leavitt@icloud.com>
2023-11-08 13:26:47 -0700

    edditing the look of elements and more learning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import { useState } from 'react'
 import './App.css';
+import {styles} from "./styles"
+
+
+
+
+function Nav() {
+  const NavList = ["Home","Admin","New Game"];
+  let isAdmin = false;
+return(
+  <ul>
+    <li style={styles.list}>
+      <button>{NavList[0]}</button>
+    </li>
+    <li style={styles.list}>
+      <button>{NavList[1]}</button>
+    </li>
+    <li style={styles.list}>
+    {isAdmin ? <button>{NavList[2]}</button> : ""}
+    </li>
+  </ul>
+);
+}
+
+
+
 
 function App() {
   const[counter, setCounter] = useState(0);
@@ -7,6 +32,7 @@ function App() {
 
   return (
     <div className="App">
+      <Nav/>
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
       <h1>{counter}</h1>
       <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
---
+++ src/styles.js
@@ -0,0 +1,36 @@
+const Container = { display: "flex", flexDirection: "column" };
+const Header = { padding: "10px 20px", textAlign: "center", color: "white" };
+const LabelContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+const Label = { padding: "10px 20px", textAlign: "center" };
+const FormContainer = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+const ErrorMessage = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+const button = {
+    color: "#3E8245"
+};
+const list = {
+    display: "inline-block",
+    marginRight: "10px",
+
+};
+export const styles = {
+  list: list,
+  button: button,
+  Container: Container,
+  Header: Header,
+  LabelContainer: LabelContainer,
+  Label: Label,
+  ErrorMessage: ErrorMessage,
+  FormContainer: FormContainer
+}
