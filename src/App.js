a0e58d6e308c7955620ce50c11d78f9415e0190c
ru55-2 <rm_leavitt@icloud.com>
2023-11-09 13:47:42 -0700

    added functions file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 import {styles} from "./styles"
+import { functions } from "./functions"
 
 
 
@@ -9,33 +10,68 @@ function Nav() {
   const NavList = ["Home","Admin","New Game"];
   let isAdmin = false;
 return(
-  <ul>
-    <li style={styles.list}>
-      <button>{NavList[0]}</button>
-    </li>
-    <li style={styles.list}>
-      <button>{NavList[1]}</button>
-    </li>
-    <li style={styles.list}>
-    {isAdmin ? <button>{NavList[2]}</button> : ""}
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li style={styles.list}>
+        <button onClick={functions.homebtn}>{NavList[0]}</button>
+      </li>
+      <li style={styles.list}>
+        <button>{NavList[1]}</button>
+      </li>
+      <li style={styles.list}>
+      {isAdmin ? <button>{NavList[2]}</button> : ""}
+      </li>
+    </ul>
+  </div>
 );
+}
+
+function Table(){
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetch("https://leavittservers.xyz/api/basketball/games")
+      .then((res) => res.json())
+      .then((json) => setGames(json));
+  }, []);
+  return(
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <th>Location Of Game</th>
+          <th>Home Team</th>
+          <th>Guest Team</th>
+        </tr>
+        </thead>
+        <tbody>
+        {games.map((game, index) => (
+            <tr key={index}>
+              <td>{game.Location}</td>
+              <td>{game.Team1}</td>
+              <td>{game.Team2}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  
+    </div>
+  );
 }
 
 
 
 
+
 function App() {
-  const[counter, setCounter] = useState(0);
-
-
   return (
     <div className="App">
       <Nav/>
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+      <Table/>
+
+
+      
+
     </div>
   );
 }
---
+++ src/functions.js
@@ -0,0 +1,7 @@
+function Home(){
+    alert("you clicked me!");
+  }
+
+  export const functions = {
+    homebtn: Home,
+  }
